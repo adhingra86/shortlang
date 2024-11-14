dbd00f241c3c3c20b6c40aaa7fda0cd700fc3284
Anish Dhingra <52705334+adhingra86@users.noreply.github.com>
2024-11-14 20:35:27 +0100

    - add heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-4xl font-bold text-center">Welcome to ShortLang</h1>
+      </div>
     </div>
   );
 }
